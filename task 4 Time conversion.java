0b10f3c020553858fe4a968b84bbf9fc7f4fffbc
vtu30102-lang <vtu30102@veltech.edu.in>
2026-09-09 10:19:26 +0530

    Create task 4 Time conversion.java

---
+++ task 4 Time conversion.java
@@ -0,0 +1,18 @@
+class Result {
+    public static String timeConversion(String s) {
+        String period = s.substring(8, 10);
+        int hour = Integer.parseInt(s.substring(0, 2));
+
+        if (period.equals("AM")) {
+            if (hour == 12) {
+                hour = 0;
+            }
+        } else {
+            if (hour != 12) {
+                hour += 12;
+            }
+        }
+
+        return String.format("%02d%s", hour, s.substring(2, 8));
+    }
+}
