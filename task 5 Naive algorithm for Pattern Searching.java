1c3f974907bbc37bcb5a7aad7fba7bc976373157
vtu30102-lang <vtu30102@veltech.edu.in>
2026-09-09 11:27:35 +0530

    Create task 5 Naive algorithm for Pattern Searching.java

---
+++ task 5 Naive algorithm for Pattern Searching.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.nextLine();
+        String pattern = sc.nextLine();
+
+        int n = text.length();
+        int m = pattern.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+
+            for (j = 0; j < m; j++) {
+                if (text.charAt(i + j) != pattern.charAt(j)) {
+                    break;
+                }
+            }
+
+            if (j == m) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
