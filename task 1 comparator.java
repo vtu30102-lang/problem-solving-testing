df9cfda60f298fdfa1e474a6123f709c5489ce55
vtu30102-lang <vtu30102@veltech.edu.in>
2026-09-09 09:43:04 +0530

    Create task 1 comparator.java

---
+++ task 1 comparator.java
@@ -0,0 +1,7 @@
+class Checker implements java.util.Comparator<Player> {
+    public int compare(Player a, Player b) {
+        if (a.score != b.score)
+            return b.score - a.score;
+        return a.name.compareTo(b.name);
+    }
+}
